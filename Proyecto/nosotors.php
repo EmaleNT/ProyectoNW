30902afa34ea4de918e05f7e3fe9fd4ddda0d3dd
Emilio Núñez Trejo <41083560+EmaleNT@users.noreply.github.com>
2023-01-12 01:01:46 -0600

    a

--- Proyecto/nosotors.php
+++ Proyecto/nosotors.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
     <link rel="stylesheet" href="css/estilosHeaderNav.css">
     <link rel="stylesheet" href="css/nosotros.css">
     <title>Mascotify</title>
