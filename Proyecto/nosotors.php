98ac3a533568680902679cc6c12007fa5a5a275e
Emilio Núñez Trejo <41083560+EmaleNT@users.noreply.github.com>
2023-01-12 02:56:56 -0600

    a

--- Proyecto/nosotors.php
+++ Proyecto/nosotors.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilosHeaderNav.css">
-    <link rel="stylesheet" href="css/nosotros.css">
+    <link rel="stylesheet" href="css/estilosHeaderNavacercade.css">
+    <!--<link rel="stylesheet" href="css/nosotros.css">-->
     <title>Mascotify</title>
 </head>
 <body>
