b93b98b777ffc427ea611dbe9c4244d164935bd1
Emilio Núñez Trejo <41083560+EmaleNT@users.noreply.github.com>
2023-01-11 21:43:53 -0600

    Add files via upload

---
+++ Proyecto/conexionDB.php
@@ -0,0 +1,64 @@
+<?php
+$db_host="localhost";
+$db_nombre="tienda";
+$db_usuario="root";
+$db_contrasena="";
+
+try {
+    $conexion=new PDO("mysql:host=$db_host;dbname=$db_nombre;",$db_usuario,$db_contrasena);
+} catch (Exception $e) {
+    echo "Ocurrió algo con la base de datos: " . $e->getMessage();
+}
+
+/*
+
+READ
+$sql="SELECT * FROM producto";
+$query=$conexion->query($sql);
+
+
+
+foreach($query as $productos){
+    echo $productos['NOMBRE'].','; 
+    echo $productos['PRECIO'].',';
+    echo $productos['CANTIDAD'];
+    echo 
+    echo '<br>'; 
+}
+
+*/
+
+/*
+UPDATE,INSTERT,DELET
+try{
+
+    $sql="UPDATE producto SET CANTIDAD='10' WHERE ID=1 ";
+    // Iniciar una transacción, desactivando 'autocommit' 
+    $conexion->beginTransaction();
+    // Cambiar el esquema de la base de datos 
+    $conexion->exec($sql);
+    // Consignar los cambios 
+    $conexion->commit();
+    echo "Se realizo con exito";    
+}
+catch(Exception $e){
+    $conexion->rollBack();
+    echo "Fallo".$e->getMessage();
+}
+*/
+
+$sql="SELECT * FROM producto";
+$query=$conexion->query($sql);
+
+
+
+foreach($query as $productos){
+    echo $productos['NOMBRE'].','; 
+    echo $productos['PRECIO'].',';
+    echo $productos['ID'];
+    echo "<img src=".$productos['']." alt='producto'>";
+    echo '<br>'; 
+}
+
+
+?>
